0317dd44d94df8f22bc48aa5058664c6473ca6d8
Pulkit0511 <pulkitgangwar0@gmail.com>
2024-04-19 20:32:01 +0530

    minor changes

--- models/Order.js
+++ models/Order.js
@@ -2,40 +2,43 @@
 const mongoose = require("mongoose");
 
 // Define the schema for the order
-const orderSchema = new mongoose.Schema({
-  orderId: {
-    type: String,
-    required: true,
-    unique: true,
+const orderSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    restaurant: {
+      type: String,
+      required: true,
+    },
+    restaurantAddress: {
+      type: String,
+      required: true,
+    },
+    customer: {
+      type: String,
+      required: true,
+    },
+    customerAddress: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["pending", "accepted", "picked up", "delivered"],
+      default: "pending",
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    // Add more fields as needed
   },
-  restaurant: {
-    type: String,
-    required: true,
-  },
-  restaurantAddress: {
-    type: String,
-    required: true,
-  },
-  customer: {
-    type: String,
-    required: true,
-  },
-  customerAddress: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["pending", "accepted", "picked up", "delivered"],
-    default: "pending",
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  // Add more fields as needed
-});
+  { timestamps: true }
+);
 
 // Create the order model
 const Order = mongoose.model("Order", orderSchema);
